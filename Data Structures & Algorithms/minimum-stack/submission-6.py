e3b1238ee666a32ee62596aaa7936487a7db600a
ajay-453 <43466570+ajay-453@users.noreply.github.com>
2026-09-01 11:40:18 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,60 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+
+    def push(self, val: int) -> None:
+        if not self.min_stack:
+          self.min_stack.append(val)
+        else:
+          val_min = min(val, self.min_stack[-1])
+          self.min_stack.append(val_min)   
+        self.stack.append(val)
+
+
+        return None
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        self.min_stack.pop()
+        return None
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+        
+    
+
+input = ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+output  = []
+
+min_stack = MinStack()
+output.append("null")
+i =1
+while i < len(input):
+    if input[i] == "push":
+        min_stack.push(input[i+1])
+        i+=1
+        output.append("null")
+        output.append("null")
+    elif input[i] == "pop":
+        min_stack.pop()
+        output.append("null")
+    elif input[i] == "top":
+        val = min_stack.top()
+        output.append(val)
+    else:
+        val = min_stack.getMin()
+        output.append(val)
+    i+=1
+
+
+
+    
